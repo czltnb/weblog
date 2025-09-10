df5816d6a1e90ca910478eeca450a06599736d9b
Teacher Egg <teacherEgg@teacheregg.com>
2025-09-10 19:26:42 +0800

    3.1 搭建 Spring Boot 多模块工程

---
+++ weblog-admin/src/test/java/com/czltnb/weblog/admin/WeblogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czltnb.weblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
