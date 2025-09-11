1c8d5bc406c0436169a0d36629c73405f81ee30a
Teacher Egg <teacherEgg@teacheregg.com>
2025-09-11 16:50:46 +0800

    3.6 Spring Boot 自定义注解，实现 API 请求日志切面

---
+++ weblog-web/src/main/java/com/czltnb/weblog/web/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.czltnb.weblog.web.controller;
+
+import com.czltnb.weblog.common.aspect.ApiOperationLog;
+import com.czltnb.weblog.web.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @PostMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    public User test(@RequestBody User user) {
+        return user;
+    }
+
+}
---
+++ weblog-web/src/main/java/com/czltnb/weblog/web/model/User.java
@@ -0,0 +1,9 @@
+package com.czltnb.weblog.web.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private Integer sex;
+}
