4ac43902c815c4fe69b2aa0899aab9452cb7020c
Teacher Egg <teacherEgg@teacheregg.com>
2025-09-11 22:05:59 +0800

    3.8 Spring Boot 实现优雅的参数校验

--- weblog-web/src/main/java/com/czltnb/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/czltnb/weblog/web/controller/TestController.java
@@ -3,10 +3,16 @@ package com.czltnb.weblog.web.controller;
 import com.czltnb.weblog.common.aspect.ApiOperationLog;
 import com.czltnb.weblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -14,8 +20,21 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        return user;
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
+        // 返参
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
+
--- weblog-web/src/main/java/com/czltnb/weblog/web/model/User.java
+++ weblog-web/src/main/java/com/czltnb/weblog/web/model/User.java
@@ -2,8 +2,23 @@ package com.czltnb.weblog.web.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class User {
+
+    @NotBlank(message = "用户名不能为空")
     private String username;
+
+    @NotNull(message = "需要填写性别性别")
     private Integer sex;
+
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18,message = "年龄必须大于18周岁")
+    @Max(value = 100,message = "年龄必须小于100周岁")
+    private Integer age;
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式必须正确")
+    private String email;
 }
