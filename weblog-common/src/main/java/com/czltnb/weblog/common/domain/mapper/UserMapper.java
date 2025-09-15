740f4b22f245e71495d9562cf23251ad890f117a
Teacher Egg <teacherEgg@teacheregg.com>
2025-09-15 20:40:57 +0800

    5.6 Spring Security 整合 JWT : 实现身份认证

--- weblog-common/src/main/java/com/czltnb/weblog/common/domain/mapper/UserMapper.java
+++ weblog-common/src/main/java/com/czltnb/weblog/common/domain/mapper/UserMapper.java
@@ -1,7 +1,14 @@
 package com.czltnb.weblog.common.domain.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.czltnb.weblog.common.domain.dos.UserDO;
 
 public interface UserMapper extends BaseMapper<UserDO> {
+    default UserDO findByUsername(String username) {
+        LambdaQueryWrapper<UserDO> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserDO::getUsername, username);
+        return selectOne(wrapper);
+    }
 }
+
